d6ae9123f18e024283e1c000a7a394245a01a6d4
LadyAlena <107322107+LadyAlena@users.noreply.github.com>
2022-07-03 18:21:20 +0300

    Changed task 02/1

--- 01/HomeworkTwoTask1.cpp
+++ 01/HomeworkTwoTask1.cpp
@@ -25,54 +25,59 @@ int main(int argc, char** argv) {
     do {
 
         std::cout << "Введите номер месяца: ";
+
         std::cin >> numberMonth;
 
-        month = static_cast<months>(numberMonth);
-
-        if (numberMonth < 13) {
-            if (month == months::januray) {
-                std::cout << "Январь" << std::endl;
-            }
-            if (month == months::february) {
-                std::cout << "Февраль" << std::endl;
-            }
-            if (month == months::march) {
-                std::cout << "Март" << std::endl;
-            }
-            if (month == months::april) {
-                std::cout << "Апрель" << std::endl;
-            }
-            if (month == months::may) {
-                std::cout << "Май" << std::endl;
-            }
-            if (month == months::june) {
-                std::cout << "Июнь" << std::endl;
-            }
-            if (month == months::july) {
-                std::cout << "Июль" << std::endl;
-            }
-            if (month == months::august) {
-                std::cout << "Август" << std::endl;
-            }
-            if (month == months::september) {
-                std::cout << "Сентябрь" << std::endl;
-            }
-            if (month == months::october) {
-                std::cout << "Октябрь" << std::endl;
-            }
-            if (month == months::november) {
-                std::cout << "Ноябрь" << std::endl;
-            }
-            if (month == months::december) {
-                std::cout << "Декабрь" << std::endl;
-            }
+        if (numberMonth < 0) {
+            std::cout << "Номер месяца не может быть отрицательным..." << std::endl;
         }
         else {
-            std::cout << "Неправильный номер!" << std::endl;
-        }
+            month = static_cast<months>(numberMonth);
 
-        if (!numberMonth) {
-            std::cout << "До свидания" << std::endl;
+            switch (month) {
+            case months::januray: 
+                std::cout << "Январь" << std::endl; 
+                break;
+            case months::february: 
+                std::cout << "Февраль" << std::endl; 
+                break;
+            case months::march:
+                std::cout << "Март" << std::endl;
+                break;
+            case months::april:
+                std::cout << "Апрель" << std::endl;
+                break;
+            case months::may:
+                std::cout << "Май" << std::endl;
+                break;
+            case months::june:
+                std::cout << "Июнь" << std::endl;
+                break;
+            case months::july:
+                std::cout << "Июль" << std::endl;
+                break;
+            case months::august:
+                std::cout << "Август" << std::endl;
+                break;
+            case months::september:
+                std::cout << "Сентябрь" << std::endl;
+                break;
+            case months::october:
+                std::cout << "Октябрь" << std::endl;
+                break;
+            case months::november:
+                std::cout << "Ноябрь" << std::endl;
+                break;
+            case months::december:
+                std::cout << "Декабрь" << std::endl;
+                break;
+            case static_cast<months>(0):
+                std::cout << "До свидания" << std::endl;
+                break;
+            default:
+                std::cout << "Неправильный номер!" << std::endl;
+                break;
+            }
         }
 
     } while (numberMonth);
