77239b24a330eb57408593d24f19e96e6a5c085c
Elena Stepanova <lady.alena-he2015@yandex.ru>
2022-07-03 16:26:36 +0300

    Added task 02/3

---
+++ 03/HomeworkTwoTask3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+struct address {
+    std::string country;
+    std::string town;
+    std::string street;
+    unsigned int homeNumber;
+    unsigned int flatNumber;
+    unsigned int index;
+};
+
+void printStruct(address* instance) {
+    std::cout << "Страна: " << instance->country << std::endl;
+    std::cout << "Город: " << instance->town << std::endl;
+    std::cout << "Улица: " << instance->street << std::endl;
+    std::cout << "Номер дома: " << instance->homeNumber << std::endl;
+    std::cout << "Номер квартиры: " << instance->flatNumber << std::endl;
+    std::cout << "Индекс: " << instance->index << std::endl;
+}
+
+int main() {
+
+    setlocale(LC_ALL, "RUS");
+
+    address currentAddress = {"Россия", "Москва", "Панфилова", 17, 45, 45678};
+
+    printStruct(&currentAddress);
+
+    std::cout << std::endl;
+
+    currentAddress = { "Россия", "Москва", "Арбат", 12, 8, 123455 };
+    printStruct(&currentAddress);
+
+    return 0;
+}
