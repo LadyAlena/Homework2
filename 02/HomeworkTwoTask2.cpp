0aea6a46ddf6dfb28f4f869ae7a98cdea79d4002
Elena Stepanova <lady.alena-he2015@yandex.ru>
2022-07-03 16:26:11 +0300

    Added task 02/2

---
+++ 02/HomeworkTwoTask2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+
+struct bankAccount {
+    int accountNumber;
+    std::string owner;
+    float balance;
+};
+
+bankAccount changeBalanceAccount(bankAccount &BankAcc, float &newBalance) {
+
+    BankAcc.balance = newBalance;
+
+    return BankAcc;
+}
+
+int main(int argc, char** argv) {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    bankAccount BankAcc;
+    float newbalance = 0.0;
+
+    std::cout << "Введите номер счета: ";
+    std::cin >> BankAcc.accountNumber;
+    std::cout << "Введите имя владельца: ";
+    std::cin >> BankAcc.owner;
+    std::cout << "Введите баланс: ";
+    std::cin >> BankAcc.balance;
+
+    std::cout << "Введите новый баланс: ";
+    std::cin >> newbalance;
+
+    changeBalanceAccount(BankAcc, newbalance);
+
+    std::cout << "Ваш счет: " << BankAcc.owner << ", " << BankAcc.accountNumber << ", " << BankAcc.balance << std::endl;
+
+    return 0;
+}
